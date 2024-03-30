4214b52bd67c4d0b485ecf15e6acc6f6ce3c7d02
Rob Pitkin <43574523+rob-pitkin@users.noreply.github.com>
2024-03-30 16:24:18 -0700

    init commit

---
+++ policy_evaluation.py
@@ -0,0 +1,12 @@
+# Implementing Policy Evaluation
+# Author: Rob Pitkin
+# Date: 03/28/24
+
+def policy_evaluation()
+
+def main():
+    print("Hello World!")
+    return
+
+if __name__ == '__main__':
+    main()
